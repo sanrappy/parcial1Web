7fde55a35a3f6bb2caa75b54f46a894171054456
sanrappy <111755845+sanrappy@users.noreply.github.com>
2024-03-13 22:33:15 -0500

    init

--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.css';
+import React from 'react';
+import { Button } from 'react-bootstrap';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
